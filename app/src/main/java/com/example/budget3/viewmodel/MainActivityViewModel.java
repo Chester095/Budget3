33d826f97c6ef2dcd433b549c3dfc98f3f520bf1
Chester095 <50018523+Chester095@users.noreply.github.com>
2021-03-01 17:29:13 +0300

    copy of budget2

---
+++ app/src/main/java/com/example/budget3/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,47 @@
+package com.example.budget3.viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.example.budget3.model.AppRepository;
+import com.example.budget3.model.Genre;
+import com.example.budget3.model.Movie;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+    AppRepository appRepository;
+    private LiveData<List<Genre>> genres;
+    private LiveData<List<Movie>> genreMovies;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        appRepository = new AppRepository(application);
+    }
+
+    public LiveData<List<Genre>> getGenres() {
+        genres = appRepository.getGenres();
+        return genres;
+    }
+
+    public LiveData<List<Movie>> getGenreMovies(int genreId) {
+        genreMovies = appRepository.getGenreMovies(genreId);
+        return genreMovies;
+    }
+
+    //метод чтобы добавлять новый фильм
+    public void addNewMovie(Movie movie) {
+        appRepository.insertMovie(movie);
+    }
+
+    public void updateMovie(Movie movie) {
+        appRepository.updateMovie(movie);
+    }
+
+    public void deleteMovie(Movie movie) {
+        appRepository.deleteMovie(movie);
+    }
+}
